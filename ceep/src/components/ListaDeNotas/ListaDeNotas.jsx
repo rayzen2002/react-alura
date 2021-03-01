2d918f81af84387cbe078c39cc812255ad66d9e8
Emanuel <emanuelzsmj@gmail.com>
2021-03-01 20:55:44 -0300

    Curso Entendo como a Biblioteca funciona - Usando State e Props

--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -6,11 +6,11 @@ class ListaDeNotas extends Component {
     render() {
         return (
             <ul className="lista-notas">
-                {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
+                {this.props.notas.map((nota, index) => {
                     return (
                         <li className="lista-notas_item" key={index}>
-                            <div>{categoria}</div>
-                            <CardNota />
+
+                            <CardNota titulo={nota.titulo} texto={nota.texto} />
                         </li>
                     );
                 })}
