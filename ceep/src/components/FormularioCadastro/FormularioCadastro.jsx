3a3be05bc7d41a31d0c9833f26f71de799a6be7c
Emanuel <emanuelzsmj@gmail.com>
2021-03-01 17:47:09 -0300

    Curso Entendo como a Biblioteca funciona - Trabalhando com Construtores, This e Bind no React

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import "./FormularioCadastro.css";
 class FormularioCadastro extends Component {
+
+    constructor() {
+        super();
+        this.titulo = "";
+    }
+    handleMudancaTitulo(event) {
+        this.titulo = event.target.value;
+    }
+
+
+
     render() {
         return (
             <form className="form-cadastro ">
@@ -8,6 +19,7 @@ class FormularioCadastro extends Component {
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
+                    onChange={this.handleMudancaTitulo.bind(this)}
                 />
                 <textarea
                     rows={15}
